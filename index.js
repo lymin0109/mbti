6fdefdd4d56f7924ae2a26d9b0cccc5368912597
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-29 14:26:36 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/', function(req,res){
+//   res.sendFile(__dirname + "/index.html")
+// })
+
+// app.listen(3000, function() {
+//   console.log("start! express server on prot 3000")
+// })
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+        res.render('index.html');
+});
+
+var server = app.listen(80, function(){
+        console.log("The server has started on port 80");
+});
+
